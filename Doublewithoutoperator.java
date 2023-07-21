9f2b592fae0e61c7fee7e5d2a99ae7a241c0c8d4
Yesswine <124653110+Yesswine@users.noreply.github.com>
2023-07-21 13:53:12 +0530

    Add files via upload

---
+++ Doublewithoutoperator.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+public class Doublewithoutoperator
+{
+  public static void main(String[] args) 
+  {
+    Scanner obj=new Scanner(System.in);
+    int n=obj.nextInt();
+    System.out.println(n<<1);
+}}

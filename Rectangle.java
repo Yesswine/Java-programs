2cd3a1ba5f6744b96ce4c9893e5c3e7ef240d983
Yesswine <124653110+Yesswine@users.noreply.github.com>
2023-07-21 14:25:42 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class Rectangle{
+public static void main(String[]args){
+
+Scanner s=new Scanner(System.in);
+
+System.out.println("enter the value of sides");
+int l=s.nextInt();
+int b=s.nextInt();
+int c=l*b;
+System.out.println("area of rectangle is:" +c);
+}
+}

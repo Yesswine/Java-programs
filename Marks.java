53fadc20ef575e83aab94bf3d5050179ea8812a9
Yesswine <124653110+Yesswine@users.noreply.github.com>
2023-07-21 14:22:43 +0530

    Add files via upload

---
+++ Marks.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class Marks{
+public static void main(String[]args){
+
+Scanner s=new Scanner(System.in);
+
+System.out.println("enter a string");
+String Y=s.nextLine();
+System.out.println("string is:" +Y);
+
+System.out.println("integer no");
+int X=s.nextInt();
+System.out.println("integer is:" +X);
+
+System.out.println("float no");
+Float Z=s.nextFloat();
+System.out.println("float is:" +Z);
+}}
+
+

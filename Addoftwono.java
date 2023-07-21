4bfa55f425a5df5338b761ed8804cff8d588290b
Yesswine <124653110+Yesswine@users.noreply.github.com>
2023-07-21 14:23:24 +0530

    Add files via upload

---
+++ Addoftwono.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class Addoftwono{
+public static void main(String[]args){
+
+Scanner s=new Scanner(System.in);
+
+System.out.println("integer no");
+int X=s.nextInt();
+int Y=s.nextInt();
+int Z=X+Y;
+System.out.println("addition of two no is:" +Z);
+}
+}
